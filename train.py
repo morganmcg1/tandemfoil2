c54660beff1c96830db1683e6700a2ea6028ff5f
Morgan McGuire <morganmcg1@users.noreply.github.com>
2026-04-29 11:56:36 +0100

    Tighter Huber loss: huber_delta=0.1 for surface pressure precision (#1121)
    * # r5-fern: huber_delta=0.1 (tighter Huber clamp for surface precision)
    
    * huber_delta=0.1: val 58.4790 (-5.04%), test 51.3554 (-5.52%)
    
    ---------
    
    Co-authored-by: senpai-charliepai2f5-fern <senpai-charliepai2f5-fern@noreply.github.com>

--- train.py
+++ train.py
@@ -773,3 +773,4 @@ else:
     _log_jsonl({"event": "done", "no_checkpoint": True, "total_train_minutes": total_time})
 
 wandb.finish()
+# r5-fern: huber_delta=0.1 (tighter Huber clamp for surface precision)
